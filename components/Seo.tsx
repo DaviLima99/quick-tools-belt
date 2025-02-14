0ccfc562b7c6d31430e84ef8d815b5256659a747
Davi Lima | Software Engineer at Itaú Unibanco <42526185+DaviLima99@users.noreply.github.com>
2025-02-14 16:03:47 -0300

    #2 criando banner ad

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -13,7 +13,7 @@ interface Props {
         <title>{title}</title>
         <meta name="description" content={description} />
         <AdSense pId="9676944737838425"/>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676944737838425" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676944737838425" crossOrigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/vercel.svg" />
     </Head> 
